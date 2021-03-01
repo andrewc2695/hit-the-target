62899503907ce47e179c8b70aef5591f1ec9884a
Andrew Curd <andrewc2695@gmail.com>
2021-03-01 14:54:36 -0800

    able to draw and move function works accordingly

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("console.log(\"webpack is working\")\n\nconst MovingObject = __webpack_require__(/*! ./moving_object.js */ \"./src/moving_object.js\")\n\nwindow.MovingObject = MovingObject;\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("console.log(\"webpack is working\")\n\nconst MovingObject = __webpack_require__(/*! ./moving_object.js */ \"./src/moving_object.js\")\n\nwindow.MovingObject = MovingObject;\n\n\n\ndocument.addEventListener(\"DOMContentLoaded\", () => {\n    const canvas = document.getElementById(\"game-canvas\")\n    canvas.width = 1000;\n    canvas.height = 600;\n    const ctx = canvas.getContext(\"2d\")\n    window.ctx = ctx;\n})\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ }),
 
@@ -25,7 +25,7 @@ eval("console.log(\"webpack is working\")\n\nconst MovingObject = __webpack_requ
   \******************************/
 /***/ ((module) => {
 
-eval("class MovingObject {\n    constructor(variables){\n        this.pos = variables.pos;\n        this.vel = variables.vel;\n        this.radius = variables.radius;\n        this.color = variables.colo;\n    }\n}\n\nmodule.exports = MovingObject;\n\n\n//# sourceURL=webpack:///./src/moving_object.js?");
+eval("class MovingObject {\n    constructor(variables){\n        this.pos = variables.pos;\n        this.vel = variables.vel;\n        this.radius = variables.radius;\n        this.color = variables.colo;\n    }\n\n    draw(ctx){\n        ctx.fillStyle = this.color;\n        ctx.beginPath();\n        ctx.arc(\n            this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true\n        );\n        ctx.fill();\n    }\n\n    move(){\n        this.pos[0] = this.pos[0] + this.vel[0]\n        this.pos[1] = this.pos[1] + this.vel[1]\n    }\n\n\n}\n\nmodule.exports = MovingObject;\n\n\n//# sourceURL=webpack:///./src/moving_object.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ console.log("webpack is working")
 const MovingObject = require("./moving_object.js")
 
 window.MovingObject = MovingObject;
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const canvas = document.getElementById("game-canvas")
+    canvas.width = 1000;
+    canvas.height = 600;
+    const ctx = canvas.getContext("2d")
+    window.ctx = ctx;
+})
