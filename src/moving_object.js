3be564262dd98bf054c7d24e722b04a988b329d0
Andrew Curd <andrewc2695@gmail.com>
2021-03-01 14:18:45 -0800

    created moving object class

---
+++ src/moving_object.js
@@ -0,0 +1,10 @@
+class MovingObject {
+    constructor(variables){
+        this.pos = variables.pos;
+        this.vel = variables.vel;
+        this.radius = variables.radius;
+        this.color = variables.colo;
+    }
+}
+
+module.exports = MovingObject;
