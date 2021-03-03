c8fea0d42a4933ef692887de1da55c990c332fc6
Andrew Curd <andrewc2695@gmail.com>
2021-03-02 21:56:57 -0800

    got robot image up

--- src/user_object.js
+++ src/user_object.js
@@ -4,15 +4,23 @@ const Util = require("./util.js");
 class UserObject extends MovingObject{
     constructor(variables){
         super(variables);
+        this.img = new Image();
+        this.img.src = "../img/robot1.png";
+
+
     }
 
     draw(ctx) {
-        ctx.fillStyle = this.color;
-        ctx.beginPath();
-        ctx.arc(
-            this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true
-        );
-        ctx.fill();
+        // ctx.fillStyle = this.color;
+        // ctx.beginPath();
+        // ctx.arc(
+        //     this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true
+        // );
+        // let img = new Image();
+        // img.src = "../img/robot1.png";
+        // img.onload = function(){
+        ctx.drawImage(this.img, this.pos[0], this.pos[1], 55, 70)
+        // }
     }
 
     readPrompts(n, prompts) {
